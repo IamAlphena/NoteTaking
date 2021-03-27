e67b87a8aa9cbc078e067a5f6ac031b7d53e5247
Alphena <alphena@gmail.com>
2021-03-26 22:53:30 -0700

    revisted methods to use global variable for better manipulation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//variables requring pieces to function
 const express = require("express");
 const path = require("path");
 const fs = require("fs")
@@ -5,6 +6,7 @@ const app = express();
 const storage = require("./db/db.json");
 const { uuid } = require('uuidv4');
 
+//directs to specific port
 const PORT = process.env.PORT || 8080
 
 //Middleware Functions
@@ -20,48 +22,70 @@ app.get("/notes", function (req, res) {
 })
 
 //API routes
-//read from notes json
+
+//continuously communiate with the json db to interact with it throughout our data
+let storedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+//pulls from storedNotes to update as we go, generating on the page
 app.get("/api/notes", function (req, res) {
-    res.json(storage)
+    res.json(storedNotes)
 });
 
+//saves a new note
 app.post("/api/notes", function (req, res) {
-
+    //creates variable to save information from app
     const notes = {
+        //generates a unique id number
         id: uuid(),
+        //pulls the title of the note
         title: req.body.title,
+        //pulls the text of the note
         text: req.body.text,
     }
 
-    fs.readFile("./db/db.json", "utf8", function (err, data) {
-        let parseData = JSON.parse(data);
-        parseData.push(notes);
-        parseData = JSON.stringify(parseData)
-        fs.writeFile("./db/db.json", parseData, function (err, data) {
-            if (err) throw err
-        })
-    })
+    //add new note to storedNotes
+    storedNotes.push(notes)
 
-
+    //stringifies the array to be able to save it 
+    storedNotes = JSON.stringify(storedNotes)
+    //saves the new note to file, or throws err
+    fs.writeFile("./db/db.json", storedNotes, function (err, data) {
+        if (err) throw err
+    });
+    //reparses the data to stay written on the page
+    storedNotes = JSON.parse(storedNotes);
+    //resolves, and logs a note was added
+    res.json(console.log("note added"))
 });
 
+//allows us to delete a note
 app.delete("/api/notes/:id", function (req, res) {
-    //Delete a note based off id
+    //creates a variable off id, pulling from the note we click on's button
     const id = req.params.id;
-    console.log(id)
 
-    fs.readFile("./db/db.json", "utf8", function (err, data) {
-        let dataNote = JSON.parse(data)
-        // console.log(dataNote)
-        for (let i = 0; i < dataNote.length; i++) {
-            if (id === dataNote[i].id){
-                dataNote.splice([i],1);
-            }else {
-                return
-            }
+    //for each item in our storedNotes array
+    storedNotes.forEach(function (data, index) {
+        //if the id we pulled matches the id of the item in the storedNotes array
+        if (id === data.id) {
+            //delete that index'd value from the array using splice
+            storedNotes.splice(data.index, 1);
+            //else, do nothing. 
+        } else {
+            return
         }
     })
-    res.end("Note was deleted")
+       //stringifies the array to be able to save it 
+       storedNotes = JSON.stringify(storedNotes)
+       //saves the new note to file, or throws err
+       fs.writeFile("./db/db.json", storedNotes, function (err, data) {
+           if (err) throw err
+       });
+       //reparses the data to stay written on the page
+       storedNotes = JSON.parse(storedNotes);
+
+       //console log that a note was deleted
+    res.end(console.log("Note was deleted"))
 });
 
+//let us know upon launch it is listening properly
 app.listen(PORT, () => console.log("App listening on port" + PORT));
